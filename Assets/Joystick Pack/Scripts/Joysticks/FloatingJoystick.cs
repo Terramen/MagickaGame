3206d23c60291be57236647404a96abe4db29694
1 1 <mrdimon4eg@gmail.com>
2020-09-28 11:16:01 +0300

    first game commit

---
+++ Assets/Joystick Pack/Scripts/Joysticks/FloatingJoystick.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class FloatingJoystick : Joystick
+{
+    //private Player_transform _playerTransform;
+    
+    protected override void Start()
+    {
+        /*_playerTransform = FindObjectOfType<Player_transform>();*/
+        base.Start();
+        background.gameObject.SetActive(false);
+    }
+
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+        background.anchoredPosition = ScreenPointToAnchoredPosition(eventData.position);
+        background.gameObject.SetActive(true);
+        base.OnPointerDown(eventData);
+        //_playerTransform.IsPressed = true;
+        // Player_transform. = true;
+    }
+    
+    public override void OnPointerUp(PointerEventData eventData)
+    {
+        
+        background.gameObject.SetActive(false);
+        base.OnPointerUp(eventData);
+       // _playerTransform.IsPressed = false;
+       // playerTransform.IsPressed = false;
+    }
+}
---
+++ Assets/Magicka/Scripts/Shooting.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Shooting : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    [SerializeField] private GameObject skill;
+    public Transform firePoint;
+    public Transform circle;
+    //[SerializeField] private float speed = 5f;
+    private Elements _elements;
+
+    void Awake()
+    {
+  //      player = GameObject.Find("/Player");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        _elements = FindObjectOfType<Elements>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("ButtonAttack"))
+        {
+            skill = GameObject.FindGameObjectWithTag("Boulder");
+            /*Debug.Log("Counter" + _elements.Counter + "Sprite" + _elements.SpriteRenderers[_elements.Counter - 1].sprite + "Sprite1" + _elements.SpriteArray[1]);
+            Debug.Log("Check" + (_elements.SpriteRenderers[_elements.Counter - 1].sprite == _elements.SpriteArray[1]));*/
+            if (_elements.Counter > 0 && _elements.SpriteRenderers[_elements.Counter - 1].sprite == _elements.SpriteArray[1])
+            {
+                Shoot();
+            }
+            foreach (var r in _elements.SpriteRenderers)
+            {
+                r.enabled = false;
+                r.sprite = _elements.SpriteArray[3];
+            }
+            _elements.Counter = 0;
+            // Debug.Log("Кнопка нажата");
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(skill, firePoint.position, firePoint.rotation);
+    }
+
+    /*private void OnTriggerEnter2D(Collider2D collider)
+    {
+        Instantiate(skill, firePoint.position, firePoint.rotation);
+        Destroy(skill);
+    }*/
+}
